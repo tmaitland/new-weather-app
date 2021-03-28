309c0f23e8df29d07904cac4fa79851b0392f973
tmaitland <tmaitland@techlaunch.io>
2021-03-28 01:55:46 -0400

    Updates

---
+++ precache-manifest.60597ec54ff16aa71722fa4eebc414ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5264877195e82d89737a0ef6c1028019",
+    "url": "/about-the-weather/index.html"
+  },
+  {
+    "revision": "170967ff50583c501a32",
+    "url": "/about-the-weather/static/css/main.cbdf42f9.chunk.css"
+  },
+  {
+    "revision": "fd0981cb43a3e303dea7",
+    "url": "/about-the-weather/static/js/2.42cbf322.chunk.js"
+  },
+  {
+    "revision": "170967ff50583c501a32",
+    "url": "/about-the-weather/static/js/main.da11f492.chunk.js"
+  },
+  {
+    "revision": "1975d7e179401f8019f5",
+    "url": "/about-the-weather/static/js/runtime~main.6b87efa2.js"
+  },
+  {
+    "revision": "123620859df8f069675ed2b66512c401",
+    "url": "/about-the-weather/static/media/forecastBG.12362085.jpg"
+  },
+  {
+    "revision": "f747b3b2c4572244737c9feb6692bb1b",
+    "url": "/about-the-weather/static/media/sunnyDay-min.f747b3b2.jpg"
+  }
+]);
