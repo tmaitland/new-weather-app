14b95a8884ca4268b85a2ea58ddf7ac11e8c457c
maitta791 <maitta791@ufl.edu>
2022-10-11 20:03:47 -0400

    Updates

---
+++ precache-manifest.b8d51ea7c18d095fb7a7c14ea404235d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90680d571f7a0252c59fbbf6a83f1f08",
+    "url": "/new-weather-app/index.html"
+  },
+  {
+    "revision": "2b19d0b6d7a0eda84e5a",
+    "url": "/new-weather-app/static/css/main.78801158.chunk.css"
+  },
+  {
+    "revision": "0f98982def123e6d4551",
+    "url": "/new-weather-app/static/js/2.1fe3800b.chunk.js"
+  },
+  {
+    "revision": "2b19d0b6d7a0eda84e5a",
+    "url": "/new-weather-app/static/js/main.472b1508.chunk.js"
+  },
+  {
+    "revision": "08a59c145890056b9cda",
+    "url": "/new-weather-app/static/js/runtime~main.145d3feb.js"
+  },
+  {
+    "revision": "123620859df8f069675ed2b66512c401",
+    "url": "/new-weather-app/static/media/forecastBG.12362085.jpg"
+  },
+  {
+    "revision": "f747b3b2c4572244737c9feb6692bb1b",
+    "url": "/new-weather-app/static/media/sunnyDay-min.f747b3b2.jpg"
+  }
+]);
